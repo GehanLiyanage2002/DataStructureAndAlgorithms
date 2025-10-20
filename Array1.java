5d46c62b756606086471fa1b9030542a9e9bfcc5
GehanLiyanage2002 <iit22002.std.uwu.ac.lk>
2025-10-20 09:11:11 +0530

    Add deletion and update functionality to Array1 class

--- Array1.java
+++ Array1.java
@@ -37,8 +37,61 @@ public class Array1 {
         }
 
 
-        scanner.close();
+        
 
+
+        System.out.println("Enter the Index do you want delete : ");
+        int deleteIndex = scanner.nextInt();
+
+        for (int i = deleteIndex; i < size; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        size--;
+
+        System.out.println("==================== After Deletion ====================");
+        for (int i = 0; i < size; i++) {
+            System.out.println("This is element " + (i+1) + ": " + arr[i]);
+        }
+
+
+        System.out.println("What a index you want to search : ");
+        int searchData = scanner.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == searchData) {
+                System.out.println("Data found at index : " + i);
+                System.out.println("Data found at position : " + (i + 1));
+                System.out.println("You searched data is : " + arr[i]);
+                break;
+            }
+        }  
+
+        System.out.println("Enter the index do you want update : ");
+        int updateIndex = scanner.nextInt();
+        System.out.println("Enter the new value : ");
+        int newValue = scanner.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            if (i == updateIndex) {
+                arr[i] = newValue;
+                System.out.println("This is element " + (i+1) + ": " + arr[i]);
+                break;
+            }
+        }
+
+        System.out.println("==================== After Updation ====================");
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("This is element " + (i+1) + ": " + arr[i]);
+        }
+
+
+
+
+
+
+        scanner.close();
 
     }
     
